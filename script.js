bb1e8da3467e07cebd0cc5cda83419045635306d
Logeshwaran <137371760+Logeshw671@users.noreply.github.com>
2023-07-26 20:27:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let myResume={
+  "basics": {
+    "name": "Logeshwaran k",
+    "email": "logeshw671@gamil.com",
+    "phone": 8778619724,
+    "degree": "B.B.A",
+    "location": {
+      "address": NO 14,15 ESHA HOMES PATHMAVATHI NAGAR poonamalli
+      "pinCode": 600056,
+      "city": "Chennai",
+      "state": "Tamilnadu",
+      "country": "India"
+    },
+    "profiles": [
+      {
+        "github":"https://github.com/Logeshw671"
+      }
+    ]
+  },
+  
+  "education": [
+    {
+      "institution": "Asan memorial college of asan and college",
+      "department": "BBA",
+      "studyType": "fulltime",
+      "batch start year": 2017,
+      "batch end year": 2020,
+    }
+  ],
+ 
+  "languages": [
+    {
+      "language": "Tamil,Enlish",
+    }
+  ],
+  "interests": [
+    {
+      "name": "script writter,youtuber,",
+    }
+  ]
+}
+console.log(myResume);
